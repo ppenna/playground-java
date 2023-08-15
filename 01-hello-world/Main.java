097ef5334780cc28e1e733d4f20c55e0bf935f75
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2023-08-15 13:34:38 -0300

    [01-hello-world] Add: Initial Example

---
+++ 01-hello-world/Main.java
@@ -0,0 +1,8 @@
+// Copyright(C) 2023 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
+// Licensed under the MIT license.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
