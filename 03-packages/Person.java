ff7e3ae5add6b54feab353a0660f074828d3b1d6
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2023-08-15 13:35:09 -0300

    [03-packages] Add: Initial Example

---
+++ 03-packages/Person.java
@@ -0,0 +1,12 @@
+// Copyright(C) 2023 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
+// Licensed under the MIT license.
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
