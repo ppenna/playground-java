f222cdbb04ef6e5a2c17668791d33529cbda2b6d
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2023-08-30 08:32:41 -0300

    [01-hello] Update: Adding Comments

--- 01-hello-world/Main.java
+++ 01-hello-world/Main.java
@@ -1,8 +1,30 @@
 // Copyright(C) 2023 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
 // Licensed under the MIT license.
 
+
+/**
+ * Main class.
+ *
+ * @note This class should have the same name of your source file.
+ *
+ * @note This is a JavaDoc comment. You can use it to comment your code,
+ * and then let Java automatically generate documentation for you.
+ * See https://docs.oracle.com/en/ to learn more.
+ */
 public class Main {
+
+	/**
+	 * Prints a message on the standard output device.
+	 *
+	 * @param args Command line arguments.
+	 */
     public static void main(String[] args) {
+
+		// Prints a string on the standard output.
         System.out.println("Hello, World!");
+
+		/*
+		 * TODO: Modify this program to print your name.
+		 */
     }
 }
